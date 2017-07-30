44081e063aab06a5ebfc288ffe450dab0b41864f
Dmitriy Beglov <ditransler@gmail.com>
2017-07-30 16:13:48 +0400

    Added examples of crating own modules

--- 13 - JavaScript Modules and Using npm/app.js
+++ 13 - JavaScript Modules and Using npm/app.js
@@ -1,7 +1,12 @@
 import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
+import apiKey from './src/config';
 
-const ages = [1, 1, 4, 52, 12, 4];
+import User, { createURL, gravatar } from './src/user';
 
-console.log(uniq(ages));
+const wes = new User('Wes Bos', 'wesbos@gmail.com', 'wesbos.com');
+const profile = createURL(wes.name);
+const image = gravatar(wes.email);
+
+console.log(wes, profile, image);
